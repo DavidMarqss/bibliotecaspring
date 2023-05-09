e0e6d996f954fbbe3e2ca23557696b5e020a7376
DavidMarques <davidestef04@gmail.com>
2023-05-09 17:26:01 +0000

    feat: insert livros

--- app/src/main/java/application/controller/LivroController.java
+++ app/src/main/java/application/controller/LivroController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import application.model.Livro;
 import application.model.LivroRepository;
 
 @Controller
@@ -17,5 +19,19 @@ public class LivroController {
     public String list(Model model){
         model.addAttribute("livros", livroRepo.findAll());
         return "WEB-INF/list.jsp";
+    }
+
+    @RequestMapping("/insert")
+    public String insert(){
+        return "WEB-INF/insert.jsp";
+    }
+
+    public String insert(@RequestParam("titulo") String titulo){
+        Livro livro = new Livro();
+        livro.setTitulo(titulo);
+
+        livroRepo.save(livro);
+        
+        return "redirect:/livro";
     }
 }
